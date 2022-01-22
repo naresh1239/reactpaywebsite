d9775f1eb7368cc542106c40029a5afae7a96bd1
Naresh kumawat <iamnareshkumawat1@gmail.com>
2022-01-22 13:43:58 +1200

    adding files

---
+++ src/pages/Contactme.jsx
@@ -0,0 +1,17 @@
+ import React from 'react'
+ import Navbar from '../components/Navbar';
+ import Footer from "../components/Footer";
+ import Contant from "../components/Contant"
+ const Contactme = () => {
+     return (
+        
+       <> 
+       <Navbar/>
+       <Contant/>
+       <Footer/>  
+        </>
+        
+     )
+ }
+
+ export default Contactme;
